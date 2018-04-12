8798a06f781f25b9eac83a8039744934e0b659fd
finley <sssrrr@vip.qq.com>
2018-04-12 17:45:42 +0800

    tab切换

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,42 @@ export default {
 </script>
 
 <style lang='scss'>
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+
 #app {
     height: 100vh;
     display: flex;
     flex-direction: column;
 }
-
+.topbar {
+    box-shadow: (0 0 5px)hsla(0, 0, 0, 0.5);
+    z-index: 1;
+}
 main {
     display: flex;
     flex: 1;
-    .editor,
+    background: rgb(247, 247, 247);
+    .editor {
+        width: 40rem;
+        margin: 20px 10px 20px 20px;
+        background: white;
+        box-shadow: 0px 0px 5px 0px rgba(50, 50, 50, 0.5);
+        border-radius: 5px;
+        overflow: hidden;
+    }
     .preview {
         flex: 1;
+        margin: 20px 20px 20px 10px;
+        background: white;
+        box-shadow: 0px 0px 5px 0px rgba(50, 50, 50, 0.5);
+        border-radius: 5px;
+        overflow: hidden;
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import './assets/reset.scss'
-import  'normalize.css'
+import 'normalize.css'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.config.devtools = true
 
 new Vue({
   el: '#app',
